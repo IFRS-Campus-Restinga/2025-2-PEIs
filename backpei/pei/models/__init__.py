f449ab67ad0b9b2fc1f41e28855c662338d3e90f
JonathasMadeira <163860898+JonathasMadeira@users.noreply.github.com>
2025-09-20 19:45:52 -0300

    Ajuste com a developer

--- backpei/pei/models/__init__.py
+++ backpei/pei/models/__init__.py
@@ -2,3 +2,5 @@ from .pessoa import *
 from .parecer import *
 from .PEIPeriodoLetivo import *
 from .professor import *
+from .pedagogo import *
+from .componenteCurricular import *
--- backpei/services/urls.py
+++ backpei/services/urls.py
@@ -4,6 +4,8 @@ from .views.dados_view_set import DadosViewSet
 from .views.PEIPeriodoLetivo_view_set import PEIPeriodoLetivoViewSet
 from .views.parecer_view_set import ParecerViewSet
 from .views.professor_view_set import ProfessorViewSet
+from .views.componenteCurricular import ComponenteCurricularViewSet
+from .views.pedagogo_view_set import PedagogoViewSet
 
 
 
@@ -12,6 +14,8 @@ router.register(r'dados', DadosViewSet, basename='dados')
 router.register(r'PEIPeriodoLetivo', PEIPeriodoLetivoViewSet, basename='PEIPeriodoLetivo')
 router.register(r'parecer', ParecerViewSet, basename='parecer')
 router.register(r'professor', ProfessorViewSet, basename='professor')
+router.register(r'componenteCurricular', ComponenteCurricularViewSet, basename='componenteCurricular')
+router.register(r'pedagogo', PedagogoViewSet, basename='pedagogo')
 app_name = 'api'
 urlpatterns = [
     path('', include(router.urls)),
