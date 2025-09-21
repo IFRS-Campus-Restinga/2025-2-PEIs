289a76b3bad7ca061d588159479d243d87ac8789
Jampier <jampierbrunetto1@gmail.com>
2025-09-21 20:52:21 -0300

    ajuste de erros

--- backpei/pei/admin.py
+++ backpei/pei/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register((Pessoa, Professor, PEIPeriodoLetivo, Parecer, Curso, Disciplina, aluno, coordenadorCurso))
+admin.site.register((Pessoa, Professor, PEIPeriodoLetivo, Parecer, Curso, Disciplina, Aluno, CoordenadorCurso))
