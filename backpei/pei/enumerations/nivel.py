86be0698421a4e88062c6f6d9e149f1bcebf7139
Gabriel Lisboa <143036931+GabrielLisboaCosta@users.noreply.github.com>
2025-09-21 22:48:05 -0300

    apagando enumerations

--- backpei/pei/enumerations/nivel.py
+++
@@ -1,7 +0,0 @@
-from django.utils.translation import gettext_lazy as _
-from django.db import models
-
-class Nivel(models.TextChoices):
-    SUPERIOR = "Superior", _("Superior")
-    MEDIO = "Ensino Médio", _("Ensino Medio")
-    NOT_INFORMED = "Não informado", _("Não informado")
