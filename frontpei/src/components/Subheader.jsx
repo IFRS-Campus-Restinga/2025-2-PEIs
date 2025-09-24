f325b1a14194514b2060c436e49116c86cbc22be
João Victor Maciel <joaovictormaciel_@hotmail.com>
2025-09-23 22:39:15 -0300

    ajustes em nomeclaturas e ignore

--- frontpei/src/components/Subheader.jsx
+++ frontpei/src/components/Subheader.jsx
@@ -11,7 +11,7 @@ const SubHeader = () => {
         <button onClick={() => navigate("/pareceres")} className="subheader-btn">Cadastro de Pareceres</button>
         <button onClick={() => navigate("/curso")} className="subheader-btn">Cadastro de Cursos</button>
         <button onClick={() => navigate("/disciplina")} className="subheader-btn">Cadastro de Disciplinas</button>
-        <button onClick={() => navigate("/periodo")} className="subheader-btn">Cadastro de Períodos Letivos</button>
+        <button onClick={() => navigate("/periodo")} className="subheader-btn">Visualizar PEI de Períodos Letivos</button>
         <button onClick={() => navigate("/peicentral")} className="subheader-btn">Visualizar PEI Central</button>
         <button onClick={() => navigate("/componentecurricular")} className="subheader-btn">Visualizar Componente Curricular</button>
         <button onClick={() => navigate("/atadeacompanhamento")} className="subheader-btn">Visualizar Ata De Acompanhamento</button>
