245f6bf76bdc965bfebea44b3db4ee87a9bf1935
Gabriel Lisboa <03-gabriel.costa@gloria.icm-sec.org.br>
2025-10-08 20:53:49 -0300

    ajustando usuario no models do log

--- backpei/logs/models.py
+++ backpei/logs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Log(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
-    #usuario = models.CharField(max_length=100, blank=True, null=True)  # Futuro: registrar usuário
+    usuario = models.CharField(max_length=100, blank=True, null=True)  # Futuro: registrar usuário
     acao = models.CharField(max_length=100)  # Apenas a ação: "Criação", "Atualização", "Exclusão"
     modelo = models.CharField(max_length=100)  # Modelo afetado: "CoordenadorCurso", "Disciplina", etc.
     objeto_id = models.IntegerField()  # ID do objeto que foi alterado
