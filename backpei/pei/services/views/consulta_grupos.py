e8a9cd51b5d4fcd935fc7fdfd83aea381c7ed6c2
maiquelhvr <67020604+maiquelhvr@users.noreply.github.com>
2025-11-20 05:44:52 -0300

    view que consulta o grupo de um usuario

---
+++ backpei/pei/services/views/consulta_grupos.py
@@ -0,0 +1,29 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework import status
+from ..permissions import BackendTokenPermission
+
+class ConsultaGrupos(APIView):
+    # nosso mecanismo de injetar token do administrador
+    permission_classes = [BackendTokenPermission]
+    def get(self, request):
+        # recebe e valida email do usuario pelo cabecalho
+        user_email = request.headers.get("X-User-Email")
+        if not user_email:
+            return Response(
+                {"erro": "Cabeçalho X-User-Email não enviado"},
+                status=status.HTTP_400_BAD_REQUEST )
+        # realiza a busca na base de usuarios
+        try:
+            usuario = User.objects.get(email=user_email)
+        except User.DoesNotExist:
+            return Response(
+                {"erro": "Usuário não encontrado"},
+                status=status.HTTP_404_NOT_FOUND )
+        # encontramos o usuario e agora pegaremos os grupos dele
+        grupos = list(usuario.groups.values_list("name", flat=True))
+        # volta a informacao
+        return Response({
+            "email": user_email,
+            "grupos": grupos })
