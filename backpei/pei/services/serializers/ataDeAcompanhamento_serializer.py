aed690b7be55cc41184388753cffa970492d7820
maiquelhvr <67020604+maiquelhvr@users.noreply.github.com>
2025-11-27 18:10:03 -0300

    atualizando pra nova realidade

--- backpei/pei/services/serializers/ataDeAcompanhamento_serializer.py
+++ backpei/pei/services/serializers/ataDeAcompanhamento_serializer.py
@@ -5,3 +5,5 @@ class AtaDeAcompanhamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = AtaDeAcompanhamento
         fields = '__all__'
+        # ator preenchido pelo backend, participantes_emails calculado
+        read_only_fields = ['ator', 'participantes_emails'] 
